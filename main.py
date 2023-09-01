54464ea70ffec15f47f01245cb9fb3086004de30
Desktop <yigit.turali@ug.bilkent.edu.tr>
2023-09-01 09:47:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+#asdasd
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
