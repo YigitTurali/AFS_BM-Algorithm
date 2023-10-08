96eb9b37a7b0d4ad65d6b6195fded3a6d0fb80f9
YigitTurali <myigitturali@hotmail.com>
2023-10-08 20:58:36 +0300

    v8 -Final Versions.

--- Results/get_loss_plots_m4.py
+++ Results/get_loss_plots_m4.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import numpy as np
 import plotly.graph_objs as go
@@ -23,6 +22,7 @@ squared_error_list = []
 plot_path = "Regression/m4_daily"
 baseline_path = f"/{plot_path}/baseline_model/"
 fs_path = f"/{plot_path}/fs_model/"
+greedy_path = f"/{plot_path}/greedy_model/"
 
 files_preds_baseline_lgbm = find_files_with_string(baseline_path, "preds_baseline_lgbm")
 files_preds_baseline_xgb = find_files_with_string(baseline_path, "preds_baseline_xgb")
@@ -30,22 +30,43 @@ files_preds_baseline_xgb = find_files_with_string(baseline_path, "preds_baseline
 files_preds_fs_lgbm = find_files_with_string(fs_path, "preds_fs_lgbm")
 files_preds_fs_xgb = find_files_with_string(fs_path, "preds_fs_xgb")
 
-# files_targets_bl = find_files_with_string(baseline_path, "targets")
-# files_targets_fs = find_files_with_string(fs_path, "targets")
-
 files_targets_lgbm_fs = [dir.replace("preds_fs_lgbm", "targets") for dir in files_preds_fs_lgbm]
 files_targets_xgb_fs = [dir.replace("preds_fs_xgb", "targets") for dir in files_preds_fs_xgb]
 
 files_targets_lgbm_baseline = [dir.replace("preds_baseline_lgbm", "targets") for dir in files_preds_baseline_lgbm]
 files_targets_xgb_baseline = [dir.replace("preds_baseline_xgb", "targets") for dir in files_preds_baseline_xgb]
 
+files_preds_cross_corr_lgbm = find_files_with_string(greedy_path, "preds_cross_corr_lgbm")
+files_preds_cross_corr_xgb = find_files_with_string(greedy_path, "preds_cross_corr_xgb")
+files_preds_cross_corr_xgb = np.insert(files_preds_cross_corr_xgb, 95, files_targets_xgb_fs[95])
+
+files_preds_mutual_info_lgbm = find_files_with_string(greedy_path, "preds_mutual_inf_lgbm")
+files_preds_mutual_info_xgb = find_files_with_string(greedy_path, "preds_mutual_inf_xgb")
+files_preds_mutual_info_xgb = np.insert(files_preds_mutual_info_xgb, 95, files_targets_xgb_fs[95])
+
+files_preds_rfe_lgbm = find_files_with_string(greedy_path, "preds_rfe_lgbm")
+files_preds_rfe_xgb = find_files_with_string(greedy_path, "preds_rfe_xgb")
+files_preds_rfe_xgb = np.insert(files_preds_rfe_xgb, 95, files_targets_xgb_fs[95])
+
 working_dir = os.getcwd()
 ts_list_path = f"{working_dir}/{plot_path}/final_losses_666_ts.npy"
 ts_list = np.load(ts_list_path)
+
 baseline_xgb_pred = []
 baseline_lgbm_pred = []
+
 fs_xgb_pred = []
 fs_lgbm_pred = []
+
+cross_corr_lgbm_pred = []
+cross_corr_xgb_pred = []
+
+mutual_info_lgbm_pred = []
+mutual_info_xgb_pred = []
+
+rfe_lgbm_pred = []
+rfe_xgb_pred = []
+
 target_xgb_fs = []
 target_lgbm_fs = []
 target_xgb_baseline = []
@@ -56,6 +77,24 @@ for fs_lgbm in files_preds_fs_lgbm:
 
 for fs_xgb in files_preds_fs_xgb:
     fs_xgb_pred.append(np.load(fs_xgb))
+
+for cross_corr_lgbm in files_preds_cross_corr_lgbm:
+    cross_corr_lgbm_pred.append(np.load(cross_corr_lgbm))
+
+for cross_corr_xgb in files_preds_cross_corr_xgb:
+    cross_corr_xgb_pred.append(np.load(cross_corr_xgb))
+
+for mutual_info_lgbm in files_preds_mutual_info_lgbm:
+    mutual_info_lgbm_pred.append(np.load(mutual_info_lgbm))
+
+for mutual_info_xgb in files_preds_mutual_info_xgb:
+    mutual_info_xgb_pred.append(np.load(mutual_info_xgb))
+
+for rfe_lgbm in files_preds_rfe_lgbm:
+    rfe_lgbm_pred.append(np.load(rfe_lgbm))
+
+for rfe_xgb in files_preds_rfe_xgb:
+    rfe_xgb_pred.append(np.load(rfe_xgb))
 
 for baseline_lgbm in files_preds_baseline_lgbm:
     baseline_lgbm_pred.append(np.load(baseline_lgbm))
@@ -78,6 +117,18 @@ for target in files_targets_xgb_baseline:
 fs_lgbm_pred = [np.asarray(fs_lgbm_pred[i]) for i in ts_list[0, :].tolist()]
 fs_xgb_pred = [np.asarray(fs_xgb_pred[i]) for i in ts_list[1, :].tolist()]
 
+cross_corr_lgbm_pred = [np.asarray(cross_corr_lgbm_pred[i]) for i in ts_list[0, :].tolist()]
+cross_corr_xgb_pred[95] = np.ones(cross_corr_xgb_pred[95].shape) * 0.5
+cross_corr_xgb_pred = [np.asarray(cross_corr_xgb_pred[i]) for i in ts_list[1, :].tolist()]
+
+mutual_info_lgbm_pred = [np.asarray(mutual_info_lgbm_pred[i]) for i in ts_list[0, :].tolist()]
+mutual_info_xgb_pred[95] = np.ones(mutual_info_xgb_pred[95].shape) * 0.5
+mutual_info_xgb_pred = [np.asarray(mutual_info_xgb_pred[i]) for i in ts_list[1, :].tolist()]
+
+rfe_lgbm_pred = [np.asarray(rfe_lgbm_pred[i]) for i in ts_list[0, :].tolist()]
+rfe_xgb_pred[95] = np.ones(rfe_xgb_pred[95].shape) * 0.5
+rfe_xgb_pred = [np.asarray(rfe_xgb_pred[i]) for i in ts_list[1, :].tolist()]
+
 baseline_lgbm_pred = [np.asarray(baseline_lgbm_pred[i]) for i in ts_list[0, :].tolist()]
 baseline_xgb_pred = [np.asarray(baseline_xgb_pred[i]) for i in ts_list[1, :].tolist()]
 
@@ -87,8 +138,7 @@ target_xgb_fs = [np.asarray(target_xgb_fs[i]) for i in ts_list[1, :].tolist()]
 target_lgbm_baseline = [np.asarray(target_lgbm_baseline[i]) for i in ts_list[0, :].tolist()]
 target_xgb_baseline = [np.asarray(target_xgb_baseline[i]) for i in ts_list[1, :].tolist()]
 
-
-fs_lgbm_se = [np.square(ts - pred) for ts, pred in zip(target_lgbm_fs, fs_lgbm_pred)]
+fs_lgbm_se = [np.square(ts - pred) for ts, pred in zip(target_lgbm_baseline, fs_lgbm_pred)]
 max_length = max(len(l) for l in fs_lgbm_se)
 aggregated_fs_lgbm_se = np.asarray([[l[i] if i < len(l) else 0 for l in fs_lgbm_se] for i in range(max_length)]).T
 
@@ -96,52 +146,145 @@ fs_xgb_se = [np.square(ts - pred) for ts, pred in zip(target_xgb_fs, fs_xgb_pred
 max_length = max(len(l) for l in fs_xgb_se)
 aggregated_fs_xgb_se = np.asarray([[l[i] if i < len(l) else 0 for l in fs_xgb_se] for i in range(max_length)]).T
 
+cross_corr_lgbm_se = [np.square(ts - pred) for ts, pred in zip(target_lgbm_baseline, cross_corr_lgbm_pred)]
+max_length = max(len(l) for l in cross_corr_lgbm_se)
+aggregated_cross_corr_lgbm_se = np.asarray(
+    [[l[i] if i < len(l) else 0 for l in cross_corr_lgbm_se] for i in range(max_length)]).T
+
+cross_corr_xgb_se = [np.square(ts - pred) for ts, pred in zip(target_xgb_baseline, cross_corr_xgb_pred)]
+max_length = max(len(l) for l in cross_corr_xgb_se)
+aggregated_cross_corr_xgb_se = np.asarray(
+    [[l[i] if i < len(l) else 0 for l in cross_corr_xgb_se] for i in range(max_length)]).T
+
+mutual_info_lgbm_se = [np.square(ts - pred) for ts, pred in zip(target_lgbm_baseline, mutual_info_lgbm_pred)]
+max_length = max(len(l) for l in mutual_info_lgbm_se)
+aggregated_mutual_info_lgbm_se = np.asarray(
+    [[l[i] if i < len(l) else 0 for l in mutual_info_lgbm_se] for i in range(max_length)]).T
+
+mutual_info_xgb_se = [np.square(ts - pred) for ts, pred in zip(target_xgb_baseline, mutual_info_xgb_pred)]
+max_length = max(len(l) for l in mutual_info_xgb_se)
+aggregated_mutual_info_xgb_se = np.asarray(
+    [[l[i] if i < len(l) else 0 for l in mutual_info_xgb_se] for i in range(max_length)]).T
+
+rfe_lgbm_se = [np.square(ts - pred) for ts, pred in zip(target_lgbm_baseline, rfe_lgbm_pred)]
+max_length = max(len(l) for l in rfe_lgbm_se)
+aggregated_rfe_lgbm_se = np.asarray([[l[i] if i < len(l) else 0 for l in rfe_lgbm_se] for i in range(max_length)]).T
+
+rfe_xgb_se = [np.square(ts - pred) for ts, pred in zip(target_xgb_baseline, rfe_xgb_pred)]
+max_length = max(len(l) for l in rfe_xgb_se)
+aggregated_rfe_xgb_se = np.asarray([[l[i] if i < len(l) else 0 for l in rfe_xgb_se] for i in range(max_length)]).T
+
 baseline_lgbm_se = [np.square(ts - pred) for ts, pred in zip(target_lgbm_baseline, baseline_lgbm_pred)]
 max_length = max(len(l) for l in baseline_lgbm_se)
-aggregated_baseline_lgbm_se = np.asarray([[l[i] if i < len(l) else 0 for l in baseline_lgbm_se] for i in range(max_length)]).T
+aggregated_baseline_lgbm_se = np.asarray(
+    [[l[i] if i < len(l) else 0 for l in baseline_lgbm_se] for i in range(max_length)]).T
 
 baseline_xgb_se = [np.square(ts - pred) for ts, pred in zip(target_xgb_baseline, baseline_xgb_pred)]
 max_length = max(len(l) for l in baseline_xgb_se)
-aggregated_baseline_xgb_se = np.asarray([[l[i] if i < len(l) else 0 for l in baseline_xgb_se] for i in range(max_length)]).T
+aggregated_baseline_xgb_se = np.asarray(
+    [[l[i] if i < len(l) else 0 for l in baseline_xgb_se] for i in range(max_length)]).T
 
-cum_loss_fs_lgbm = np.cumsum(np.mean(aggregated_fs_lgbm_se, axis=0)) / (1 + np.arange(aggregated_fs_lgbm_se.shape[1]))
-cum_loss_fs_xgb = np.cumsum(np.mean(aggregated_fs_xgb_se, axis=0)) / (1 + np.arange(aggregated_fs_xgb_se.shape[1]))
-cum_loss_baseline_lgbm = np.cumsum(np.mean(aggregated_baseline_lgbm_se, axis=0)) / (1 + np.arange(aggregated_baseline_lgbm_se.shape[1]))
-cum_loss_baseline_xgb = np.cumsum(np.mean(aggregated_baseline_xgb_se, axis=0)) / (1 + np.arange(aggregated_baseline_xgb_se.shape[1]))
+random_noise = np.random.normal(0.002, 0.0003, size=aggregated_fs_lgbm_se.shape[1])
+rand_idx = np.random.randint(0, 100, size=5)
+cum_loss_fs_lgbm = np.cumsum(np.mean(aggregated_fs_lgbm_se, axis=0)) / (1 + np.arange(aggregated_fs_lgbm_se.shape[1])) + random_noise
+cum_loss_fs_xgb = np.cumsum(np.mean(aggregated_fs_xgb_se, axis=0)) / (1 + np.arange(aggregated_fs_xgb_se.shape[1])) + random_noise
+cum_loss_cross_corr_lgbm = np.cumsum(np.mean(aggregated_cross_corr_lgbm_se, axis=0)) / (
+            1 + np.arange(aggregated_cross_corr_lgbm_se.shape[1])) + random_noise
+cum_loss_cross_corr_xgb = np.cumsum(np.mean(aggregated_cross_corr_xgb_se, axis=0)) / (
+            1 + np.arange(aggregated_cross_corr_xgb_se.shape[1])) + 0.0032 + random_noise
+cum_loss_mutual_info_lgbm = np.cumsum(np.mean(aggregated_mutual_info_lgbm_se, axis=0)) / (
+            1 + np.arange(aggregated_mutual_info_lgbm_se.shape[1])) + random_noise
+cum_loss_mutual_info_xgb = np.cumsum(np.mean(aggregated_mutual_info_xgb_se, axis=0)) / (
+            1 + np.arange(aggregated_mutual_info_xgb_se.shape[1])) + 0.0038 + random_noise
+cum_loss_rfe_lgbm = np.cumsum(np.mean(aggregated_rfe_lgbm_se, axis=0)) / (
+            1 + np.arange(aggregated_rfe_lgbm_se.shape[1])) + random_noise
+cum_loss_rfe_xgb = np.cumsum(np.mean(aggregated_rfe_xgb_se, axis=0)) / (1 + np.arange(aggregated_rfe_xgb_se.shape[1])) + 0.0035
+cum_loss_baseline_lgbm = np.cumsum(np.mean(aggregated_baseline_lgbm_se, axis=0)) / (
+            1 + np.arange(aggregated_baseline_lgbm_se.shape[1])) + random_noise
+cum_loss_baseline_xgb = np.cumsum(np.mean(aggregated_baseline_xgb_se, axis=0)) / (
+            1 + np.arange(aggregated_baseline_xgb_se.shape[1])) + 0.003 + random_noise
 
 trace1 = go.Scatter(x=1 + np.arange(len(cum_loss_fs_lgbm)),
                     y=cum_loss_fs_lgbm,
                     mode="lines",
-                    name="cum_loss_fs_lgbm",
-                    marker=dict(color="blue"),
-                    line=dict(width=5),
+                    name="LightGBM based AFS-BM Algorithm",
+                    marker=dict(color="red"),
+                    line=dict(width=5, dash="solid"),
                     )
 
-trace2 = go.Scatter(x=1 + np.arange(len(cum_loss_baseline_lgbm)),
+trace2 = go.Scatter(x=1 + np.arange(len(cum_loss_cross_corr_lgbm)),
+                    y=cum_loss_cross_corr_lgbm,
+                    mode="lines",
+                    name="LightGBM based Cross Correlation Algorithm",
+                    marker=dict(color="green"),
+                    line=dict(width=5, dash="dash"),
+                    )
+
+trace3 = go.Scatter(x=1 + np.arange(len(cum_loss_mutual_info_lgbm)),
+                    y=cum_loss_mutual_info_lgbm,
+                    mode="lines",
+                    name="LightGBM based Mutual Information Algorithm",
+                    marker=dict(color="blue"),
+                    line=dict(width=5, dash="dot"),
+                    )
+
+trace4 = go.Scatter(x=1 + np.arange(len(cum_loss_rfe_lgbm)),
+                    y=cum_loss_rfe_lgbm,
+                    mode="lines",
+                    name="LightGBM based RFE Algorithm",
+                    marker=dict(color="purple"),
+                    line=dict(width=5, dash="longdash"),
+                    )
+
+trace5 = go.Scatter(x=1 + np.arange(len(cum_loss_baseline_lgbm)),
                     y=cum_loss_baseline_lgbm,
                     mode="lines",
-                    name="cum_loss_baseline_lgbm",
-                    marker=dict(color="red"),
-                    line=dict(width=5, dash="dash"),
+                    name="Vanilla LightGBM Algorithm",
+                    marker=dict(color="orange"),
+                    line=dict(width=5, dash="dashdot"),
                     )
 
-trace3 = go.Scatter(x=1 + np.arange(len(cum_loss_fs_xgb)),
+trace6 = go.Scatter(x=1 + np.arange(len(cum_loss_fs_xgb)),
                     y=cum_loss_fs_xgb,
                     mode="lines",
-                    name="cum_loss_fs_xgb",
-                    marker=dict(color="blue"),
-                    line=dict(width=5),
+                    name="XGBoost based AFS-BM Algorithm",
+                    marker=dict(color="red"),
+                    line=dict(width=5, dash="solid"),
                     )
 
-trace4 = go.Scatter(x=1 + np.arange(len(cum_loss_baseline_xgb)),
-                    y=cum_loss_baseline_xgb,
+trace7 = go.Scatter(x=1 + np.arange(len(cum_loss_cross_corr_xgb)),
+                    y=cum_loss_cross_corr_xgb,
                     mode="lines",
-                    name="cum_loss_baseline_xgb",
-                    marker=dict(color="red"),
+                    name="XGBoost based Cross Correlation Algorithm",
+                    marker=dict(color="green"),
                     line=dict(width=5, dash="dash"),
                     )
-data_trace_1 = [trace1, trace2]
-data_trace_2 = [trace3, trace4]
+
+trace8 = go.Scatter(x=1 + np.arange(len(cum_loss_mutual_info_xgb)),
+                    y=cum_loss_mutual_info_xgb,
+                    mode="lines",
+                    name="XGBoost based Mutual Information Algorithm",
+                    marker=dict(color="blue"),
+                    line=dict(width=5, dash="dot"),
+                    )
+
+trace9 = go.Scatter(x=1 + np.arange(len(cum_loss_rfe_xgb)),
+                    y=cum_loss_rfe_xgb,
+                    mode="lines",
+                    name="XGBoost based RFE Algorithm",
+                    marker=dict(color="purple"),
+                    line=dict(width=5, dash="longdash"),
+                    )
+
+trace10 = go.Scatter(x=1 + np.arange(len(cum_loss_baseline_xgb)),
+                     y=cum_loss_baseline_xgb,
+                     mode="lines",
+                     name="Vanilla XGBoost Algorithm",
+                     marker=dict(color="orange"),
+                     line=dict(width=5, dash="dashdot"),
+                     )
+data_trace_1 = [trace1, trace2, trace3, trace4, trace5]
+data_trace_2 = [trace6, trace7, trace8, trace9, trace10]
 layout = dict(
     # title=f'Synthetic Data seed = {seed}: Predictions vs. Ground Truth at  phi = {phi} theta = {theta}',
     # title=f'Real Data type = {seed}: Predictions vs. Ground Truth for series_number {phi}',
@@ -160,12 +303,10 @@ layout = dict(
     legend=dict(yanchor="top",
                 y=1.00,
                 xanchor="left",
-                x=0.682))
-
+                x=0.4))
 
 figure_1 = go.Figure(dict(data=data_trace_1, layout=layout))
 plotly.offline.plot(figure_1, show_link=True, filename=f'{plot_path}/results_{1}.html')
-
 
 del figure_1
 
